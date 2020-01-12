d1757b980fd93d923cf857d3c13220bcd14f7ef7
hui-ho <hui-ho@outlook.com>
2020-01-12 15:16:32 +0800

    Post Repository

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -2,10 +2,9 @@
 
 namespace devprojoh\Press\Console;
 
-use devprojoh\Press\Post;
 use devprojoh\Press\Facades\Press;
+use devprojoh\Press\Repositories\PostRepository;
 use Illuminate\Console\Command;
-use Illuminate\Support\Str;
 
 class ProcessCommand extends Command
 {
@@ -13,24 +12,23 @@ class ProcessCommand extends Command
 
     protected $description = 'Updates blog posts.';
 
-    public function handle()
+    public function handle(PostRepository $postRepository)
     {
         if (Press::configNotPublished()) {
             return $this->warn('Please publish the config file by running ' .
                 '\'php artisan vendor:publish --tag=press-config\'');
         }
 
-        $posts = Press::driver()->fetchPosts();
-
         try {
+
+            $posts = Press::driver()->fetchPosts();
+
+            $this->info('Number of Posts: ' . count($posts));
+
             foreach ($posts as $post) {
-                Post::create([
-                    'identifier' => $post['identifier'],
-                    'slug' => Str::slug($post['title']),
-                    'title' => $post['title'],
-                    'body' => $post['body'],
-                    'extra' => $post['extra'] ?? [],
-                ]);
+                $postRepository->save($post);
+
+                $this->info('Post: ' . $post['title']);
             }
         } catch (\Exception $e) {
             $this->error($e->getMessage());
