18680b94fe002fd95125a39ef392871f27b0970b
hui-ho <hui-ho@outlook.com>
2020-01-12 09:47:02 +0800

    Config File & Laravel Publishable Package Assets

---
+++ src/Console/ProcessCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace devprojoh\Press\Console;
+
+use devprojoh\Press\Post;
+use devprojoh\Press\PressFileParser;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class ProcessCommand extends Command
+{
+    protected $signature = 'press:process';
+
+    protected $description = 'Updates blog posts.';
+
+    public function handle()
+    {
+        if (is_null(config('press'))) {
+            return $this->warn('Please publish the config file by running ' .
+                '\'php artisan vendor:publish --tag=press-config\'');
+        }
+
+        $files = File::files(config('press.path'));
+
+        // Process each file
+        foreach ($files as $file) {
+            $post = (new PressFileParser($file->getPathname()))->getData();
+        }
+
+        // Persist to the DB
+        Post::create([
+            'identifier' => Str::random(),
+            'slug' => Str::slug($post['title']),
+            'title' => $post['title'],
+            'body' => $post['body'],
+            'extra' => $post['extra'] ?? [],
+        ]);
+    }
+}
--- src/PressBaseServiceProvider.php
+++ src/PressBaseServiceProvider.php
@@ -2,21 +2,36 @@
 
 namespace devprojoh\Press;
 
+use devprojoh\Press\Console\ProcessCommand;
 use Illuminate\Support\ServiceProvider;
 
 class PressBaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->registerPublishing();
+        }
+
         $this->registerResource();
     }
 
     public function register()
     {
+        $this->commands([
+            ProcessCommand::class,
+        ]);
     }
 
     private function registerResource()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/press.php' => config_path('press.php'),
+        ], 'press-config');
     }
 }
